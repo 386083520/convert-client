7ed8948e3103b5c7d03d947ed9cacee41954db9e
386083520 <386083520@qq.com>
2020-06-27 22:19:32 +0800

    部分修改

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -4,19 +4,33 @@
       文件上传转换
     </div>
     <el-upload
+      v-if="transStates === '1'"
       class="upload-input"
       drag
       :action="filePath"
+      :data="fileInfo"
       :on-success="uploadSuccess"
       multiple>
       <i class="el-icon-upload"></i>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
       <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
     </el-upload>
-    <div v-if="uploadFilePath">
+    <div class="loading" v-else-if="transStates === '2'">
+      <i class="loading-gif"></i>
+      <div class="loading-info">
+        正在转换
+      </div>
+    </div>
+    <div class="load-finished" v-else>
+      <i class="loaded-gif"></i>
+      <div class="loaded-info">
+        转换完成
+      </div>
+    </div>
+    <div v-if="uploadFileFinished">
       <el-button @click="convertFile()">开始转换</el-button>
     </div>
-    <div v-if="downloadFilePath">
+    <div v-if="downloadFileFinished">
       <el-button @click="downloadFile()">开始下载</el-button>
     </div>
   </div>
@@ -29,38 +43,69 @@ export default {
   name: 'UploadFile',
   mounted () {
     console.log('gsd', this.$route.query.type)
+    this.fileInfo.convertType = this.$route.query.type
+    this.fileInfo.uuid = this.genUuid()
   },
   data () {
     return {
       filePath: CONFIG.UPLOAD_FILE,
-      uploadFilePath: '',
-      downloadFilePath: ''
+      uploadFileFinished: false,
+      downloadFileFinished: false,
+      transStates: '1', // 1：未转换 2：转换中 3：转换完成
+      fileInfo: {
+        uuid: '', // 标识本次上传的信息
+        convertType: '' // 转换类型
+      },
+      percent: 0
     }
   },
   methods: {
     uploadSuccess (response, file, fileList) {
       if (response.status === 1) {
-        this.uploadFilePath = response.body
+        this.uploadFileFinished = true
       }
     },
+    genUuid () {
+      let s = []
+      let hexDigits = '0123456789abcdef'
+      for (let i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
+      }
+      s[14] = '4'
+      s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1)
+      s[8] = s[13] = s[18] = s[23] = '-'
+      this.uuidA = s.join('')
+      return this.uuidA
+    },
     async convertFile () {
-      if (this.uploadFilePath) {
+      this.transStates = '2'
+      this.percent = '0'
+      if (this.fileInfo.uuid) {
         let params = {
-          filePath: this.uploadFilePath,
-          convertType: this.$route.query.type
+          uuid: this.fileInfo.uuid
         }
         let convertRes = await API.convertFile(params)
-        console.log('gsd2', convertRes)
         if (convertRes.data.status === 1) {
-          this.uploadFilePath = ''
-          this.downloadFilePath = convertRes.data.body
+          if (convertRes.data.body !== '') {
+            this.uploadFileFinished = false
+            this.downloadFileFinished = true
+            this.transStates = '3'
+            this.$message.success('转换文件成功，总耗时：' + convertRes.data.body + '（s）')
+          } else {
+            this.$message.error('转换文件异常')
+          }
+        } else {
+          this.$message.error('转换文件异常')
         }
-        console.log('gsd', convertRes)
       }
     },
     downloadFile () {
-      let url = CONFIG.DOWNLOAD_FILE + '?filePath=' + this.downloadFilePath
+      let url = CONFIG.DOWNLOAD_FILE + '?uuid=' + this.fileInfo.uuid
       window.open(url)
+      this.uploadFileFinished = false
+      this.downloadFileFinished = false
+      this.transStates = '1'
+      this.fileInfo.uuid = this.genUuid()
     }
   }
 }
@@ -74,6 +119,32 @@ export default {
     }
     .upload-input {
       margin: 20px;
+    }
+    .loading {
+      margin: 20px;
+      text-align: center;
+      .loading-gif {
+        width: 80px;
+        height: 80px;
+        display: inline-block;
+        background: url("../assets/progress_convert.gif") no-repeat center;
+      }
+      .loading-info {
+        margin-bottom: 20px;
+      }
+    }
+    .load-finished {
+      margin: 20px;
+      text-align: center;
+      .loaded-gif {
+        width: 80px;
+        height: 80px;
+        display: inline-block;
+        background: url("../assets/finish.png") no-repeat center;
+      }
+      .loaded-info {
+        margin-bottom: 20px;
+      }
     }
   }
 </style>
